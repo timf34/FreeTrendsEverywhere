49d0a3a1d542e744b99bfd1c2e32c4acf62642ce
timf34 <farrelti@tcd.ie>
2023-01-18 10:38:20 +0000

    added simple starter pytrends script

---
+++ src/pytrends_processing.py
@@ -0,0 +1,15 @@
+from pytrends.request import TrendReq
+
+pytrends = TrendReq(hl='en-US', tz=360)
+
+kw_list = ["Blockchain"]
+pytrends.build_payload(kw_list, cat=0, timeframe='today 5-y', geo='', gprop='')
+
+df = pytrends.interest_over_time()
+df.head()
+
+# Path: src\pytrends_processing.py
+import matplotlib.pyplot as plt
+
+plt.plot(df.index, df['Blockchain'])
+plt.show()
