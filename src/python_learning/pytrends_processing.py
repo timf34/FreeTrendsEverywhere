f392fa818ac661c9f1459fb393994065df89d704
timf34 <farrelti@tcd.ie>
2023-01-20 23:51:53 +0000

    refactored as a class

--- src/python_learning/pytrends_processing.py
+++ src/python_learning/pytrends_processing.py
@@ -1,19 +1,41 @@
-from pytrends.request import TrendReq
-
-pytrends = TrendReq(hl='en-US', tz=360)
-
-kw_list = ["Blockchain"]
-pytrends.build_payload(kw_list, cat=0, timeframe='today 5-y', geo='', gprop='')
-
-df = pytrends.interest_over_time()
-df.head()
-
-# Path: src\pytrends_processing.py
+import pandas as pd
 import matplotlib.pyplot as plt
 
-plt.plot(df.index, df['Blockchain'])
-plt.savefig('data/blockchain.png')
-plt.show()
+from pytrends.request import TrendReq
+from typing import List, Dict
 
-# Save the data to a CSV file
-df.to_csv("data/blockchain.csv")
+
+class PyTrendsProcessing:
+    # Docs here: https://github.com/GeneralMills/pytrends
+    def __init__(self, timeframe: str = 'today 1-y', ):
+        self.pytrends = TrendReq(hl='en-US', tz=360)
+        self.timeframe: str = timeframe
+
+    def get_data(self, kw_list: List[str]) -> pd.DataFrame:
+        self.pytrends.build_payload(kw_list=kw_list, timeframe=self.timeframe)
+        return self.pytrends.interest_over_time()  # dataframe
+
+    @staticmethod
+    def save_data(_df: pd.DataFrame) -> None:
+        _df.to_csv("data/blockchain.csv")
+
+    @staticmethod
+    def plot_data(df: pd.DataFrame, title: str, x_label: str, y_label: str, file_name: str) -> None:
+        plt.plot(df.index, df[title])
+        plt.xlabel(x_label)
+        plt.ylabel(y_label)
+        plt.savefig(file_name)
+        plt.show()
+
+
+def main():
+    pytrends = PyTrendsProcessing()
+    df = pytrends.get_data(['blockchain'])
+    pytrends.save_data(df)
+    pytrends.plot_data(df, 'blockchain', 'Date', 'Interest', 'blockchain.png')
+    # pytrends.plot_data(df, 'bitcoin', 'Date', 'Interest', 'bitcoin.png')
+    # pytrends.plot_data(df, 'ethereum', 'Date', 'Interest', 'ethereum.png')
+
+
+if __name__ == '__main__':
+    main()
