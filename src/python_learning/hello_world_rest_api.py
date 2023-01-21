873f705f0538eae309f93f03e3c80c6ffda0c592
timf34 <farrelti@tcd.ie>
2023-01-21 14:57:53 +0000

    added svg endpoint for testing

--- src/python_learning/hello_world_rest_api.py
+++ src/python_learning/hello_world_rest_api.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, Response, send_file
 from flask_cors import CORS
 
 from pytrends_processing import PyTrendsProcessing
@@ -13,10 +13,13 @@ pt = PyTrendsProcessing()
 def hello():
     query_word = request.json['searchQuery']
     data = pt.prepare_data(keyword_list=[query_word])
-    print(data)
-    # query_word = {"hello": "my bitches"}
-    # TODO: note that even this doesn't work! We're just not allowed to make an external web request rn.
     return jsonify(message=f'Hello, {data}!')
+
+
+@app.route('/graph.svg')
+def graph():
+    with open('test.svg', 'rb') as f:
+        return Response(f.read(), content_type='image/svg+xml')
 
 
 if __name__ == '__main__':
