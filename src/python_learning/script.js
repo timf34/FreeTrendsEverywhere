f927b0089596d1876f77e5a2eef893e6b3ff3b58
timf34 <farrelti@tcd.ie>
2023-01-21 15:11:48 +0000

    working for .svg but sizing is way off

--- src/python_learning/script.js
+++ src/python_learning/script.js
@@ -1,20 +1,46 @@
 function run () {
 
-    const searchQuery = "finance";
+    // Create a div for the svg file
+    var div = document.createElement("div");
+    div.id = "svg_div";
+    document.body.appendChild(div);
 
-    fetch('http://localhost:5000/', {
+    // Create a svg file
+    var svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+    svg.setAttribute("width", "100%");
+    svg.setAttribute("height", "100%");
+    svg.setAttribute("viewBox", "0 0 100 100");
+    svg.setAttribute("preserveAspectRatio", "xMidYMid meet");
+    svg.setAttribute("id", "svg");
+    document.getElementById("svg_div").appendChild(svg);
+
+
+
+
+    // Fetch the .svg file from the Python app at local host 5000
+
+    fetch('http://localhost:5000/graph.svg', {
         method: 'POST',
         headers: {
-            'Content-Type': 'application/json'
+            'Content-Type': 'image/svg+xml'
         },
-        body: JSON.stringify({
-            searchQuery: searchQuery
-        })
+        body: svg
     })
-        .then(response => response.json())
-        .then(data => {
-            console.log(data.message); // "Hello, World!"
-        });
+    .then(response => response.text())
+    .then(data => {
+        console.log(data);
+        // Add the svg file to the div
+        var svg = document.getElementById("svg");
+        svg.innerHTML = data;
+    }
+    )
+    .catch((error) => {
+        console.error('Error:', error);
+    }
+    );
+
+
+
 
 }
 
