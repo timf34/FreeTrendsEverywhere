2844b705accfb1a83c53377400414214322d12eb
timf34 <farrelti@tcd.ie>
2023-01-21 00:00:12 +0000

    converted to class and fixed + added Note

--- src/python_learning/pytrends_processing.py
+++ src/python_learning/pytrends_processing.py
@@ -6,36 +6,34 @@ from typing import List, Dict
 
 
 class PyTrendsProcessing:
-    # Docs here: https://github.com/GeneralMills/pytrends
-    def __init__(self, timeframe: str = 'today 1-y', ):
+    # Docs here:
+    def __init__(self, timeframe: str = "today 5-y"):
         self.pytrends = TrendReq(hl='en-US', tz=360)
-        self.timeframe: str = timeframe
+        self.timeframe = timeframe  # Note: setting this to "today 1-y" will return an error. Leave for now.
 
     def get_data(self, kw_list: List[str]) -> pd.DataFrame:
         self.pytrends.build_payload(kw_list=kw_list, timeframe=self.timeframe)
         return self.pytrends.interest_over_time()  # dataframe
 
     @staticmethod
-    def save_data(_df: pd.DataFrame) -> None:
-        _df.to_csv("data/blockchain.csv")
+    def data_to_json(df: pd.DataFrame) -> str:
+        return df.to_json(orient='index')
 
     @staticmethod
-    def plot_data(df: pd.DataFrame, title: str, x_label: str, y_label: str, file_name: str) -> None:
+    def plot_data(df: pd.DataFrame, title: str, x_label: str, y_label: str) -> None:
         plt.plot(df.index, df[title])
         plt.xlabel(x_label)
         plt.ylabel(y_label)
-        plt.savefig(file_name)
         plt.show()
 
 
 def main():
-    pytrends = PyTrendsProcessing()
-    df = pytrends.get_data(['blockchain'])
-    pytrends.save_data(df)
-    pytrends.plot_data(df, 'blockchain', 'Date', 'Interest', 'blockchain.png')
-    # pytrends.plot_data(df, 'bitcoin', 'Date', 'Interest', 'bitcoin.png')
-    # pytrends.plot_data(df, 'ethereum', 'Date', 'Interest', 'ethereum.png')
+    pytrends_processing = PyTrendsProcessing()
+    df = pytrends_processing.get_data(["Blockchain"])
+    pytrends_processing.plot_data(df, "Blockchain", "Date", "Interest")
 
 
 if __name__ == '__main__':
     main()
+
+
